3bd9485b618f28a27fde82bfcb8b67d1cf364b1a
jgiwinski <juliaiwinski@gmail.com>
2021-02-01 17:22:27 -0700

    Create Turn files

---
+++ src/Turn.js
@@ -0,0 +1,20 @@
+class Turn {
+  constructor(guess) {
+    this.guess = guess;
+    // this.card = NEW CARD
+  }
+  returnGess() {
+
+  }
+  returnCard() {
+
+  }
+  evaluateGuess() {
+
+  }
+  giveFeedback() {
+
+  }
+}
+
+module.exports = Turn
